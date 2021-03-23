9c5853a3158b439241a5225c584d80bde8ef869b
Michael Hegner <hegner123@gmail.com>
2021-03-23 09:50:52 -0400

    added attributes to each span

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # social-slider
-An interactive slider element
+An interactive slider element.
+
+## MVP
+* Create a document to move dots from left to right based on 3 slider's position.
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 
 window.onload = function() {
 let dot = document.querySelectorAll('#dots .qodef-dot');
+let age = [24,26,34,30,80,50,40,46,59,88,70,64,52,43,34,29,14,16,19,22,34];
+let location = ["New York","Los Angeles", "Miami", "Houston", "Richmond"];
+let occupation = ["Plumber","Web Developer", "Construction", "Teacher"];
 
-dot[4].style.background = "blue";
+function randomNumber(arrayLength){
+ return   Math.floor(Math.random() * arrayLength);
+}
 
+for (i = 0; i < dot.length; i++){
 
+let dataObject = {
+    objAge: age[randomNumber(age.length)],
+    objLocation: location[randomNumber(location.length)],
+    objOccupation: occupation[randomNumber(occupation.length)]
+}
+
+dot[i].setAttribute("age", dataObject.objAge);
+dot[i].setAttribute("location", dataObject.objLocation);
+dot[i].setAttribute("occupation", dataObject.objOccupation);
+
+}
+
+console.log(dot);
 
 }
